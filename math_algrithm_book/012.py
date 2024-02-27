934930186d6478d5bf09f16707d00c8016edd7c5
AbQma <68310896+abukuma777@users.noreply.github.com>
2024-02-27 21:30:02 +0900

    math_algrithm_book chapter3

---
+++ math_algrithm_book/012.py
@@ -0,0 +1,15 @@
+import math
+
+N = int(input())
+flag = False
+
+for i in range(2, int(math.sqrt(N)) + 1):
+    if N % i == 0:
+        flag = True
+        break
+
+
+if flag == True:
+    print("No")
+else:
+    print("Yes")

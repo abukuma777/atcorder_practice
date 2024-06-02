a861c19f2b7735100422ff059840b562470c5045
AbQma <68310896+abukuma777@users.noreply.github.com>
2024-06-02 19:20:30 +0900

    math_algrithm_book chapter1

---
+++ math_algrithm_book/chapter2/003.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N, result = 0;
+    cin >> N;
+    int a[N]; // 要素Nの配列
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> a[i];
+        result += a[i];
+    }
+
+    cout << result << endl;
+    return 0;
+}

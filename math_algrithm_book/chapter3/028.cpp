7fb1f3a6826126d75a8f64453951be6053884cf2
AbQma <68310896+abukuma777@users.noreply.github.com>
2024-06-11 10:00:51 +0900

    math_algrithm_book chapter3-7

---
+++ math_algrithm_book/chapter3/028.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // input
+    int N;
+    cin >> N;
+    int H[N], dp[N];
+
+    for (int i = 1; i <= N; i++)
+        cin >> H[i];
+
+    for (int i = 1; i <= N; i++)
+    {
+        if (i == 1)
+            dp[i] = 0;
+        if (i == 2)
+            dp[i] = abs(H[i - 1] - H[i]);
+        if (i >= 3)
+        {
+            int v1 = dp[i - 1] + abs(H[i - 1] - H[i]); // 1個前の足場からジャンプするとき
+            int v2 = dp[i - 2] + abs(H[i - 2] - H[i]); // 2 個前の足場からジャンプするとき
+            dp[i] = min(v1, v2);
+        }
+    }
+    cout << dp[N] << endl;
+    return 0;
+}

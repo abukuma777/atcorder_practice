13396fe2a1958b506c0c7ef0e7af0f89e82c3c65
AbQma <68310896+abukuma777@users.noreply.github.com>
2024-06-27 08:49:54 +0900

    dp_contest a review

---
+++ template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 最大の更新
+template <class T>
+inline bool chmax(T &a, T b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+// 最小の更新
+template <class T>
+inline bool chmin(T &a, T b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+    return 0;
+}
